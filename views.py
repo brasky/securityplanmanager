77be5582d5d2bbc54430bb5bea41d567296152fc
Elliot DeMatteis <edematteis@firstinfotech.com>
2018-12-17 04:44:18 +0000

    cleaned up views file

--- views.py
+++ views.py
@@ -16,7 +16,6 @@ def get_all_controls():
 
 def index(request):
 	args = {}
-
 	return render(request, 'search_box.html', args)	
 
 def search(request):
@@ -24,19 +23,9 @@ def search(request):
 		search_text = request.POST['search_text']
 	else:
 		search_text = ''
-#	controls = Control.objects.filter(control_text__contains=search_text)
-#	print(controls)	
 	all_controls = get_all_controls()
 	print(search_text)	
-
 	results = process.extract(search_text, all_controls, limit=10, scorer=fuzz.partial_ratio)
-	
-
-#	controls = process.extract(search_text, all_controls,limit=5 )
-	
-
-			
-#	print(controls)
 	return render_to_response('ajax_search.html', {'controls' : results})
 
 
