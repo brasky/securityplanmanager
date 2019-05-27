9936c18269fc12d371a71d54f7ee36caf12cde20
Elliot DeMatteis <elliotdematteis@gmail.com>
2019-05-26 17:21:10 -0700

    tweaks to the nav control search

--- static/ssp_dev/nav-search.js
+++ static/ssp_dev/nav-search.js
@@ -16,10 +16,36 @@ $(function(){
     });
 });
 
-$("#nav-search-results").focusout(function(){
-    $('#nav-search-results').html();
-    
-});
+$(function() {
+    $(".nav-search-focus").blur(function(){
+     setTimeout(function(){
+        var $focused = document.activeElement.id;
+        if($focused == "nav-search" || $focused == "nav-search-results"){
+            jQuery.noop
+        }else {
+            $('#nav-search-results').empty();
+        }
+        },100);
+    });
+   });
+
+//    $(function(){
+//     $('#nav-search').keyup(function() {
+//     	if($('#nav-search').val()) {
+
+//         $.ajax({
+//             type: "POST",
+//             url: "/nav-search/",
+//             data: { 
+//                 'search_text' : $('#nav-search').val(),
+//                 'csrfmiddlewaretoken' : $("input[name=csrfmiddlewaretoken]").val()
+//             },
+//             success: searchSuccess,
+//             dataType: 'html'
+//         });
+//     } else { $('#nav-search-results').empty();  } 
+//     });
+// });
 
 function searchSuccess(data, textStatus, jqXHR)
 {
